21a748212c0e1e9a358245d547a7e4c19afd850f
Stefan Waldmann <stefan.waldmann@doubleslash.de>
2022-12-17 15:48:25 +0100

    Solve day 4 part 2

--- src/day04.rs
+++ src/day04.rs
@@ -1,23 +1,38 @@
 mod solution {
     use std::ops::RangeInclusive;
+    use crate::day04::solution::Solution::*;
 
     const INPUT: &'static str = include_str!("input04.txt");
 
-    fn solve(input: &str) -> usize {
+    enum Solution {
+        PartOne,
+        PartTwo,
+    }
+
+    fn solve(input: &str, part: Solution) -> usize {
         let mut count = 0;
         let lines = input.split("\n").into_iter();
         for line in lines {
             let mut sp = line.split(",");
             let r1 = to_range(sp.next());
             let r2 = to_range(sp.next());
-            if r1.start() >= r2.start() && r1.end() <= r2.end() {
-                count += 1;
-            } else if r1.start() <= r2.start() && r1.end() >= r2.end() {
-                count += 1;
-            }
+            match part {
+                PartOne => if contains(r1, r2) { count += 1 },
+                PartTwo => if overlaps(r1, r2) { count += 1 },
+            }            
         }
         count
     }
+
+    fn overlaps(r1: RangeInclusive<usize>, r2: RangeInclusive<usize>) -> bool {
+        r1.start() <= r2.end() && r2.start() <= r1.end()
+    }
+
+    fn contains(r1: RangeInclusive<usize>, r2: RangeInclusive<usize>) -> bool {
+        (r1.start() >= r2.start() && r1.end() <= r2.end()) ||
+            (r1.start() <= r2.start() && r1.end() >= r2.end())
+    }
+
 
     fn to_range(str: Option<&str>) -> RangeInclusive<usize> {
         // println!("{:?}", str);
@@ -40,14 +55,22 @@ mod solution {
 
         #[test]
         fn test_solve_part_1() {
-            assert_eq!(2, solve(TEST_INPUT));
+            assert_eq!(2, solve(TEST_INPUT, PartOne));
+        }
+
+        #[test]
+        fn test_solve_part_2() {
+            assert_eq!(4, solve(TEST_INPUT, PartTwo));
         }
 
         #[test]
         fn do_solve_part_1() {
-            assert_eq!(431, solve(INPUT));
+            assert_eq!(431, solve(INPUT, PartOne));
         }
 
+        #[test]
+        fn do_solve_part_2() {
+            assert_eq!(823, solve(INPUT, PartTwo));
+        }
     }
-
 }
