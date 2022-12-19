7294a674727b380a592a6b0164997acae1bdf89e
Stefan Waldmann <stefan.waldmann@doubleslash.de>
2022-12-19 09:13:31 +0100

    Solve day 6 part 2

--- src/day06.rs
+++ src/day06.rs
@@ -4,12 +4,20 @@ mod solution {
 
     const INPUT: &'static str = include_str!("input06.txt");
 
-    fn solve(input: &str) -> Option<usize> {
+    fn solve_part_1(input: &str) -> Option<usize> {
+        solve(input, 4)
+    }
+
+    fn solve_part_2(input: &str) -> Option<usize> {
+        solve(input, 14)
+    }
+
+    fn solve(input: &str, distinct_chars: usize) -> Option<usize> {
         let mut last_chars: HashSet<char> = HashSet::new();
-        for i in 4..input.len() {
-            let _ = &input[i-4..i].chars().for_each(|c| { last_chars.insert(c); () });
+        for i in distinct_chars..input.len() {
+            let _ = &input[i-distinct_chars..i].chars().for_each(|c| { last_chars.insert(c); () });
             // println!("{} {:?}", i, last_chars);
-            if last_chars.len() == 4 {
+            if last_chars.len() == distinct_chars {
                 return Some(i);
             }
             last_chars.clear();
@@ -23,16 +31,30 @@ mod solution {
 
         #[test]
         fn test_solve_part_1() {
-            assert_eq!(Some(7), solve("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
-            assert_eq!(Some(5), solve("bvwbjplbgvbhsrlpgdmjqwftvncz"));
-            assert_eq!(Some(6), solve("nppdvjthqldpwncqszvftbrmjlhg"));
-            assert_eq!(Some(10), solve("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
-            assert_eq!(Some(11), solve("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+            assert_eq!(Some(7), solve_part_1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+            assert_eq!(Some(5), solve_part_1("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+            assert_eq!(Some(6), solve_part_1("nppdvjthqldpwncqszvftbrmjlhg"));
+            assert_eq!(Some(10), solve_part_1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+            assert_eq!(Some(11), solve_part_1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+        }
+
+        #[test]
+        fn test_solve_part_2() {
+            assert_eq!(Some(19), solve_part_2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+            assert_eq!(Some(23), solve_part_2("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+            assert_eq!(Some(23), solve_part_2("nppdvjthqldpwncqszvftbrmjlhg"));
+            assert_eq!(Some(29), solve_part_2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+            assert_eq!(Some(26), solve_part_2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
         }
 
         #[test]
         fn do_solve_part_1() {
-            assert_eq!(Some(1262), solve(INPUT));
+            assert_eq!(Some(1262), solve_part_1(INPUT));
+        }
+
+        #[test]
+        fn do_solve_part_2() {
+            assert_eq!(Some(3444), solve_part_2(INPUT));
         }
 
     }
