df38f6166a2717f26c01e97a83b92b501db44772
Stefan Waldmann <stefan.waldmann@doubleslash.de>
2022-12-21 12:39:29 +0100

    Solve day 8 part 2

--- src/day08.rs
+++ src/day08.rs
@@ -1,8 +1,10 @@
 #[allow(dead_code)]
 mod solution {
+    use std::cmp::max;
+
     const INPUT: &'static str = include_str!("input08.txt");
 
-    fn solve(input: &str) -> usize {
+    fn solve_part_1(input: &str) -> usize {
         let (height_map, height, width) = parse(input);
         // println!("{:?}", height_map);
         // println!("height={} width={}", height, width);
@@ -78,10 +80,28 @@ mod solution {
         visible_count
     }
 
-    fn print_map(map: &Vec<Vec<bool>>) {
-        for i in 0..map.len() {
-            println!("{:?}", map[i])
-        };
+    fn solve_part_2(input: &str) -> usize {
+        let (height_map, height, width) = parse(input);
+        let mut max_score: usize = 0;
+        for y in 1..height-1 {
+            for x in 1..width-1 {
+                // look up
+                let up_steps = (1..y).rev().collect();
+                let view_dist_up = get_view_dist_y_dir(&height_map, up_steps, y, x);
+                // look down
+                let down_steps = (y+1..height-1).collect();
+                let view_dist_down = get_view_dist_y_dir(&height_map, down_steps, y, x);
+                // look left
+                let left_steps = (1..x).rev().collect();
+                let view_dist_left = get_view_dist_x_dir(&height_map, left_steps, y, x);
+                // look right
+                let right_steps = (x+1..width-1).collect();
+                let view_dist_right = get_view_dist_x_dir(&height_map, right_steps, y, x);
+                let score: usize = view_dist_up * view_dist_down * view_dist_left * view_dist_right;
+                max_score = max(score, max_score);
+            }
+        }
+        max_score
     }
 
     fn parse(input: &str) -> (Vec<Vec<u8>>, usize, usize) {
@@ -98,6 +118,36 @@ mod solution {
         let height = map.len();
         let width = map[0].len();
         (map, height, width)
+    }
+
+    fn get_view_dist_y_dir(height_map: &Vec<Vec<u8>>, steps_range: Vec<usize>, y: usize, x: usize) -> usize {
+        let mut view_dist = 1;
+        for y1 in steps_range {
+            if height_map[y][x] > height_map[y1][x] {
+                view_dist += 1;
+            } else {
+                break;
+            }
+        }
+        view_dist
+    }
+
+    fn get_view_dist_x_dir(height_map: &Vec<Vec<u8>>, steps_range: Vec<usize>, y: usize, x: usize) -> usize {
+        let mut view_dist = 1;
+        for x1 in steps_range {
+            if height_map[y][x] > height_map[y][x1] {
+                view_dist += 1;
+            } else {
+                break;
+            }
+        }
+        view_dist
+    }
+
+    fn print_map(map: &Vec<Vec<bool>>) {
+        for i in 0..map.len() {
+            println!("{:?}", map[i])
+        };
     }
 
     #[cfg(test)]
@@ -125,12 +175,22 @@ mod solution {
 
         #[test]
         fn test_solve_part_1() {
-            assert_eq!(21, solve(TEST_INPUT))
+            assert_eq!(21, solve_part_1(TEST_INPUT))
+        }
+
+        #[test]
+        fn test_solve_part_2() {
+            assert_eq!(8, solve_part_2(TEST_INPUT))
         }
 
         #[test]
         fn do_solve_part_1() {
-            assert_eq!(1840, solve(INPUT))
+            assert_eq!(1840, solve_part_1(INPUT))
+        }
+
+        #[test]
+        fn do_solve_part_2() {
+            assert_eq!(405769, solve_part_2(INPUT))
         }
     }
 }
