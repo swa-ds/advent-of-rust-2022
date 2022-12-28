5336691a9078b02c2b1cd3306ea1b2848c3e971b
Stefan Waldmann <stefan.waldmann@doubleslash.de>
2022-12-28 16:06:16 +0100

    Solve day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,8 +1,7 @@
 mod solution {
-
     const INPUT: &'static str = include_str!("input10.txt");
-    
-    fn solve(input: &str) -> i32 {
+
+    fn solve_part_1(input: &str) -> i32 {
         let instructions = parse(input);
         let mut signal_strength = 0;
         let mut x = 1;
@@ -13,14 +12,14 @@ mod solution {
                 ("noop", None) => {
                     cycle += 1;
                     calc_signal_strength(&mut signal_strength, cycle, x);
-                },
+                }
                 ("addx", Some(val)) => {
                     cycle += 1;
                     calc_signal_strength(&mut signal_strength, cycle, x);
                     cycle += 1;
                     calc_signal_strength(&mut signal_strength, cycle, x);
                     x += val;
-                },
+                }
                 i => panic!("Invalid instruction {:?}", i)
             }
         }
@@ -36,6 +35,47 @@ mod solution {
             // println!("cycle={}, x={}, signal strength={}", cycle, x, cycle * x)
         }
     }
+
+    fn solve_part_2(input: &str) -> String {
+        let instructions = parse(input);
+        let mut x = 1;
+        let mut cycle = 0;
+        let mut output: String = String::new();
+
+        for instruction in instructions {
+            match instruction {
+                ("noop", None) => {
+                    draw_pixel(cycle, x, &mut output);
+                    cycle += 1;
+                }
+                ("addx", Some(val)) => {
+                    draw_pixel(cycle, x, &mut output);
+                    cycle += 1;
+                    draw_pixel(cycle, x, &mut output);
+                    cycle += 1;
+                    x += val;
+                }
+                i => panic!("Invalid instruction {:?}", i)
+            }
+        }
+        output
+    }
+
+    fn draw_pixel(cycle: i32, x: i32, output: &mut String) {
+        let pos = cycle % 40;
+        let pixel;
+        if pos >= x-1 && pos <= x+1 {
+            pixel = "#";
+        } else {
+            pixel = ".";
+        }
+        // println!("cycle={} x={} pos={} pixel={}", cycle, x, pos, pixel);
+        *output += pixel;
+        if pos == 39 {
+            *output += "\n";
+        }
+    }
+
 
     fn parse(input: &str) -> Vec<(&str, Option<i32>)> {
         let mut instructions = vec![];
@@ -56,16 +96,30 @@ mod solution {
     mod tests {
         use super::*;
 
-        const TEST_INPUT: &'static str = include_str!("input10_test.txt");
+        const TEST_INPUT:    &'static str = include_str!("input10_test.txt");
+        const EXPECTED_TEST: &'static str = include_str!("input10_expected_test.txt");
+        const EXPECTED_P2:   &'static str = include_str!("input10_expected_part_2.txt");
 
         #[test]
         fn test_solve_part_1() {
-            assert_eq!(13140, solve(TEST_INPUT));
+            assert_eq!(13140, solve_part_1(TEST_INPUT));
         }
         
         #[test]
+        fn test_solve_part_2() {
+            assert_eq!(EXPECTED_TEST, solve_part_2(TEST_INPUT));
+        }
+
+        #[test]
         fn do_solve_part_1() {
-            assert_eq!(12980, solve(INPUT));
+            assert_eq!(12980, solve_part_1(INPUT));
+        }
+        
+        #[test]
+        fn do_solve_part_2() {
+            let output = solve_part_2(INPUT);
+            println!("{}", output);
+            assert_eq!(EXPECTED_P2, output);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,4 @@ mod day10;
 
 fn main() {
     println!("{}", "Hello Advent of Code 2022 (in Rust! 🦀)");
-    let mut modify = 0;
-    modify_it(&mut modify);
-    println!("{}", modify);
-}
-
-fn modify_it(m: &mut i32) {
-    *m += 5;
 }
